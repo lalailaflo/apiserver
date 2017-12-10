e81ea0fa8a664f26814a70494f58434ae7bc7e38
camila zamora <lalaila_flo@MacBook-Pro-de-camila.local>
2017-12-10 16:39:18 -0300

    added CORS to create_a_task

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,5 +1,6 @@
-var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+const mongoose = require('mongoose');
+
+const Task = mongoose.model('Tasks');
 
 exports.list_all_tasks = (req, res) => {
   Task.find({}, (err, task) => {
@@ -13,12 +14,16 @@ exports.list_all_tasks = (req, res) => {
   });
 };
 
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
+exports.create_a_task = (req, res) => {
+  const newTask = new Task(req.body);
+  newTask.save((err, task) => {
+    if (err) {
       res.send(err);
-    res.json(task);
+    } else {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.json(task);
+    }
   });
 };
 
