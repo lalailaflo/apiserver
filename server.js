627df99524b1f3c81e9b90aed33b8e7774837736
camila zamora <lalaila_flo@MacBook-Pro-de-camila.local>
2017-12-10 14:35:25 -0300

    corregido algunas paginas con eslint y agregado CORS

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3001,
-   mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'), //created model loading here
-  bodyParser = require('body-parser');
+const app = require('express')();
+const mongoose = require('mongoose');
+const Task = require('./api/models/todoListModel'); // created model loading here
+const bodyParser = require('body-parser');
+const routes = require('./api/routes/todoListRoutes');
+
+const port = process.env.PORT || 3001;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://localhost/Tododb');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+routes(app); // register the route
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
-routes(app); //register the route
-
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+app.use((req, res) => {
+  res.status(404).send({ url: `${req.originalUrl} not found` });
 });
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log(`todo list RESTful API server started on:   ${port}`);
