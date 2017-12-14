24b4c0b2efbc9105101a94070d7a0eadd23032c5
camila zamora <lalaila_flo@MacBook-Pro-de-camila.local>
2017-12-13 23:50:41 -0300

    agrega soporte cross origin en server.js y metodo post - create_a_task

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ const bodyParser = require('body-parser');
 const routes = require('./api/routes/todoListRoutes');
 
 const port = process.env.PORT || 3001;
-
+const allowCrossDomain = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  } else {
+    next();
+  }
+};
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb');
 
-
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app); // register the route
-
-app.use((req, res) => {
-  res.status(404).send({ url: `${req.originalUrl} not found` });
-});
-
 app.listen(port);
 
 console.log(`todo list RESTful API server started on:   ${port}`);
